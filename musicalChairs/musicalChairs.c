d7632ae4ae0b910d398da527d0072067aec4530a
w4rb0y <iamnj95@gmail.com>
2018-09-07 22:55:26 +0530

    Question completed. Premitive version1(Pv.1)

--- musicalChairs/musicalChairs.c
+++ musicalChairs/musicalChairs.c
@@ -1,3 +1,8 @@
+//Practicing circular Queue
+/*
+Notes: We won't use front just work using rear.
+*/
+//fronter files
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
@@ -7,142 +12,196 @@
 typedef struct node{
   int data;
   struct node *next;
-} NODE;
+} node;
 
 int count = 0;
 
-void main()
+void enqueue(node **front, node **rear, int element)
 {
-  void enqueue(NODE **, int);
-  void dequeue(NODE **, int);
-  void readQueue(NODE *);
-  int randomGenerate(int);
-  //taking input from the file
-
-  FILE *fp;
-  if((fp = fopen(INPUTFILE, "r")) == NULL)
-    {
-      perror("InputFile ");
-      exit(1);
-    }
-
-  int n; // number of elements
-  int buffer; // var to hold elements comming from the file
-
-  NODE *head = NULL;
-
-  fscanf(fp, "%d", &n);
- printf("The number of elements are: %d \n", n);
-
-  for (int i = 0; i < n; i++)
-    {
-      fscanf(fp, "%d", &buffer);
-      enqueue(&head,buffer);
-    }
-
-  printf("The elements are: ");
-
-  NODE *trace = head; // another var to move in the queue
-
-  // Reading all the elements of the queue.
-
-  readQueue(head);
-
-  // generating random number
-  int elementsLeft = n;
-  for(int j = 1; j < n; j++)
-    {
-  int unfortunateGuy;
-  unfortunateGuy = randomGenerate(elementsLeft);
-
-  printf("\n The unfortunateGuy is : %d \n", unfortunateGuy+1);
-
-  // moving to the unfortunate guy.
-
-  /*
-  trace = head;
-  for(int i = 0; i < unfortunateGuy; i++)
-    {
-    trace = trace->next;
-    }
-  */
-
-  // deleting that element and freeing the memory.
-  dequeue(&head, unfortunateGuy);
-  count = 0;
-  readQueue(head);
-  elementsLeft--;
-    }
-  printf("\n The winner is : %d \n", head->data);
-}
-
-void enqueue(NODE **ptr, int element)
+  if(*front == NULL && *rear == NULL)
   {
-    if(*ptr == NULL)
-      {
-	//	printf("\n The queue is empty.Inserting first element. : %d \n", element);
-	NODE *new = (NODE*) malloc(sizeof(NODE));
-	new->data = element;
-	new->next = NULL;
-	*ptr = new;
-	return;
-      }
-    else if((*ptr)->next == NULL)
-      {
-	//	printf("\n Only one element present. Adding element. : %d \n", element);
-	NODE *new = (NODE*) malloc(sizeof(NODE));
-	(*ptr)->next = new;
-	new->data = element;
-	new->next = NULL;
-	return;
-      }
-    else if((*ptr) != NULL && (*ptr)->next != NULL)
-      {
-	//	printf("\n Seems like there are already elements in the queue! Adding : %d \n", element);
-	NODE *traceEnd = *ptr;
-	while(traceEnd->next != NULL)
-	  traceEnd = traceEnd->next;
-	NODE *new = (NODE*) malloc(sizeof(NODE));
-	traceEnd->next = new;
-	new->data = element;
-	new->next = NULL;
-	return;
-      }
-    printf("Enqueue Failed! You should die! ");
+    printf("\n The queue is empty.Inserting first element. : %d \n", element);
+    *rear = (node*) malloc(sizeof(node));
+    (*rear)->data = element;
+    (*rear)->next = NULL;
+    *front = *rear;
+    return;
   }
-
-
-void dequeue(NODE **ptr, int position)
-{
-  NODE *traceEnd = *ptr;
-  NODE *prev = NULL;
-  if(position == 0)
-    *ptr = (*ptr)->next;
- else //moving to unfortunate guy
-   {
-     for(int i = 0; i < position; i++)
-    {
-    prev = traceEnd;
-    traceEnd = traceEnd->next;
-    }
-  prev->next = traceEnd->next;
-   }
-  free(traceEnd);
+  else if (*front == *rear)//else if((*front)->next == NULL)
+  {
+    printf("\n Only one element present. Adding element. : %d \n", element);
+    *rear = (node*) malloc(sizeof(node));
+    (*front)->next = *rear;
+    (*rear)->data = element;
+    (*rear)->next = *front;
+    return;
+  }
+  else if(*front != *rear)//else if((*front) != NULL && (*front)->next != NULL)
+  {
+    printf("\n Seems like there are already elements in the queue! Adding : %d \n", element);
+    node *new = (node*) malloc(sizeof(node));
+    (*rear)->next = new;
+    new->data = element;
+    new->next = NULL;
+    *rear = new;
+    (*rear)->next = *front;
+    return;
+  }
+  printf("Enqueue Failed! You should die! ");
 }
 
-void readQueue(NODE *trace)
+void readQueue(node *front, node *rear)
 {
-    while(trace != NULL)
-    {
-      printf("%d ", trace->data);
-      trace = trace->next;
-      count++;
-    }
-    printf("\n Total no of elements left: %d \n", count);
+  count = 0;
+  node *temp = front;
+  while(temp != rear)
+  {
+    printf("%d ", temp->data);
+    temp = temp->next;
+    count++;
+  }
+  printf("%d",temp->data);
+  count++;
+  printf("\n Total no of elements left: %d \n", count);
 }
+
+
+void circularRead(node *front, int position)
+{
+  node *temp = front;
+  for(int i = 0; i < position; i++)
+  temp = temp->next;
+
+  node *trace = temp;
+  do
+  {
+    printf("%d ", trace->data);
+    trace = trace->next;
+  }while(trace != temp);
+}
+
+
+void dequeue(node **front, node **rear, int position)
+{
+  // if(*front == *rear)
+  // {
+  //   *front = NULL;
+  //   free(*rear);
+  //   *rear = NULL;
+  //   return;
+  // }
+  if(*front != *rear)
+  {
+    node *temp = *front;
+    node *prev = *front;
+    if(!position)
+    {
+      printf("0th position given.\n");
+      *front = (*front)->next;
+      free(temp);
+      return;
+    }
+    while(position)
+    {
+      prev = temp;
+      temp = temp->next;
+      position--;
+    }
+    if(temp == *rear)
+    {
+      printf("last position given.\n");
+      prev->next = *front;
+      free(*rear);
+      *rear = prev;
+      return;
+    }
+    printf("\n Removing given node. \n");
+    // while(temp->next != *rear)
+    //   temp = temp->next;
+    prev->next = temp->next;
+    free(temp);
+    return;
+  }
+  else
+  printf("Trying to dequeue last node too.!");
+}
+
 
 int randomGenerate(int n)
 {
   srand(time(NULL));
   return(rand() % n);
+}
+
+
+void main()
+{
+  /* Taking input from the file */
+
+  FILE *fp;
+  if((fp = fopen(INPUTFILE, "r")) == NULL)
+  {
+    perror("InputFile ");
+    exit(1);
+  }
+
+  int n; // number of elements
+  int buffer; // var to hold elements comming from the file
+
+  fscanf(fp, "%d", &n);
+  printf("The number of elements are: %d \n", n);
+
+  node *front = NULL;
+  node *rear = NULL;
+
+  for (int i = 0; i < n; i++)
+  {
+    fscanf(fp, "%d", &buffer);
+    enqueue(&front,&rear, buffer);
+  }
+
+  printf("The elements are: ");
+
+  // Reading all the elements of the queue.
+
+  readQueue(front,rear);
+
+char test;
+
+  int elementsLeft = n;
+  for(int j = 1; j < n; j++)
+  {
+    int unfortunateGuy;
+    unfortunateGuy = randomGenerate(elementsLeft);
+
+    printf("\n The unfortunateGuy is : %d \n", unfortunateGuy+1);
+
+    // moving to the unfortunate guy.
+
+    // deleting that element and freeing the memory.
+    dequeue(&front,&rear, unfortunateGuy);
+      //scanf("%c", &test);
+    readQueue(front, rear);
+    printf("\n");
+
+    //circularRead(front,unfortunateGuy);
+    elementsLeft--;
+  }
+  printf("\n The winner is : %d \n", front->data);
+
+
+
+
+
+
+
+  //
+  // printf("\n The position from where you want to read the data? \n ");
+  // int position;
+  // scanf("%d",&position);
+  //
+  // dequeue(&front,&rear,position-1);
+  //
+  // readQueue(front,rear);
+  // circularRead(front,position-1);
 }
